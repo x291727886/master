0addcd469aade107bc51248d6297186629bc1b79
j-xiajirong-jk <xiajirong-jk@360jinrong.net>
2019-07-15 15:51:29 +0800

    初始化(*^_^*)

---
+++ src/main/java/com/xiajr/masterblog/MasterBlogApplication.java
@@ -0,0 +1,13 @@
+package com.xiajr.masterblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiajr/masterblog/MasterBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiajr.masterblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
